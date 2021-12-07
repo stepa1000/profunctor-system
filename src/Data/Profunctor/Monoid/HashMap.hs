a7c2062b7b6a81415b634e4821c26a7a99e0494d
Anisimkov Stepan <stepa100023@gmail.com>
2021-12-08 01:56:14 +0600

    fix HashMap.

--- src/Data/Profunctor/Monoid/HashMap.hs
+++ src/Data/Profunctor/Monoid/HashMap.hs
@@ -31,46 +31,46 @@ import Control.Lens
 --class Hashable h => WithHash a h | a -> h, h -> a where
 --  getHashable :: a -> h  WithHash (p a b) h =>
 
-data ProHashMap h a b p x y = ProHashMap
-  {unProHashMap :: HashMap h (Body a b p x y)}
+data ProHashMap h p x y = ProHashMap
+  {unProHashMap :: HashMap h (p x y)}
 
-instance Profunctor (ProHashMap h a b p) where
+instance Profunctor p => Profunctor (ProHashMap h p) where
   dimap fl fr (ProHashMap hm) = ProHashMap $ fmap (dimap fl fr) hm
 
-instance (Hashable h, Eq h) => ProSemigroup (ProHashMap h a b p) where
+instance (Hashable h, Eq h) => ProSemigroup (ProHashMap h p) where
   (ProHashMap hm1) <<>> (ProHashMap hm2) = ProHashMap (hm1 <> hm2)
 
-instance (Hashable h, Eq h) => ProMonoid (ProHashMap h a b p) where
+instance (Hashable h, Eq h) => ProMonoid (ProHashMap h p) where
   proMempty = ProHashMap mempty
   proMappend = (<<>>)
   proMconcat = Prelude.foldr (<<>>) proMempty
 
-hositProHashMap :: (Eq h, Eq h2, Hashable h, Hashable h2)
-              => (HashMap h (Body a b p x y) -> HashMap h2 (Body a2 b2 p2 x2 y2))
-              -> ProHashMap h a b p x y
-              -> ProHashMap h2 a2 b2 p2 x2 y2
+hositProHashMap :: (Eq h, Eq h2, Hashable h, Hashable h2,Profunctor p)
+              => (HashMap h (p x y) -> HashMap h2 (p2 x2 y2))
+              -> ProHashMap h p x y
+              -> ProHashMap h2 p2 x2 y2
 hositProHashMap f (ProHashMap hm) = ProHashMap $ f hm
 
 proHashMapTo :: (Eq h)
-             => (HashMap h (Body a b p x y) -> z)
-             -> ProHashMap h a b p x y
+             => (HashMap h (p x y) -> z)
+             -> ProHashMap h p x y
              -> z
 proHashMapTo f (ProHashMap hm) = f hm
 
 singletonPHM :: (Eq h, Hashable h)
              => h
-             -> Body a b p x y
-             -> ProHashMap h a b p x y
+             -> p x y
+             -> ProHashMap h p x y
 singletonPHM h b = ProHashMap $ singleton h b
 
 sizePHM :: (Eq h)
-        => ProHashMap h a b p x y -> Int
+        => ProHashMap h p x y -> Int
 sizePHM = proHashMapTo Data.HashMap.Lazy.size
 
 lookupPHM :: (Hashable h, Eq h)
-           => h -> ProHashMap h a b p x y -> Maybe (Body a b p x y)
+           => h -> ProHashMap h p x y -> Maybe (p x y)
 lookupPHM h = proHashMapTo (Data.HashMap.Lazy.lookup h)
 
 isoProHashMap :: (Eq h)
-              => Iso' (ProHashMap h a b p x y) (HashMap h (Body a b p x y))
+              => Iso' (ProHashMap h p x y) (HashMap h (p x y))
 isoProHashMap = iso unProHashMap ProHashMap
